0533a67533033010a8241133961b4c3d6a1d3493
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-10-02 22:12:35 +0100

    update

--- backend/pugorugh/models.py
+++ backend/pugorugh/models.py
@@ -2,6 +2,20 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+CHOICES = {
+    'age': (('B', 'baby'),
+            ('Y', 'young'),
+            ('A', 'adult'),
+            ('S', 'senior')),
+    'gender': (('M', 'male'),
+               ('F', 'female')),
+    'size': (('S', 'small'),
+             ('M', 'medium'),
+             ('L', 'large'),
+             ('XL', 'extra large'))
+}
+
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     image_filename = models.CharField(max_length=100)
@@ -9,10 +23,10 @@ class Dog(models.Model):
                              default='unknown')
     age = models.IntegerField(help_text='Months in number')
     gender = models.CharField(
-        max_length=20,
+        max_length=1,
         help_text='"m" for male, "f" for female, "u" for unknown')
     size = models.CharField(
-        max_length=20,
+        max_length=2,
         help_text='"s" for small, "m" for medium, "l" for large, '
                   '"xl" for extra large, "u" for unknown')
 
@@ -24,7 +38,7 @@ class UserDog(models.Model):
     user = models.ForeignKey(User)
     dog = models.ForeignKey(Dog)
     status = models.CharField(
-        max_length=10, help_text='"l" for liked, "d" for disliked',
+        max_length=1, help_text='"l" for liked, "d" for disliked',
         default='u')
 
     def __str__(self):
@@ -35,14 +49,22 @@ class UserDog(models.Model):
 class UserPref(models.Model):
     user = models.OneToOneField(User)
     age = models.CharField(
-        max_length=20,
+        default='B,Y,A,S',
+        choices=CHOICES['age'],
+        max_length=7,
         help_text='"b" for baby, "y" for young, '
                   '"a" for adult, "s" for senior')
-    gender = models.CharField(max_length=10,
-                              help_text='"m" for male, "f" for female')
-    size = models.CharField(max_length=10,
-                            help_text='"s" for small, "m" for medium,'
-                                      '"l" for large, "xl" for extra')
+    gender = models.CharField(
+        default='M,F',
+        choices=CHOICES['gender'],
+        max_length=3,
+        help_text='"m" for male, "f" for female')
+    size = models.CharField(
+        default='S,M,L,XL',
+        choices=CHOICES['size'],
+        max_length=8,
+        help_text='"s" for small, "m" for medium,'
+                  '"l" for large, "xl" for extra')
 
     def __str__(self):
         return "{}'s dog preferences".format(self.user.username.title())
--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth import get_user_model
-
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
 
-from . import serializers
+from .serializers import UserSerializer
 
 
 class UserRegisterView(CreateAPIView):
     permission_classes = (permissions.AllowAny,)
     model = get_user_model()
-    serializer_class = serializers.UserSerializer
+    serializer_class = UserSerializer
