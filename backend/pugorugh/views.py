d888893714bf06e64cafbbfd8a75861e7f48b303
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-10-14 21:38:33 +0100

    update

--- backend/pugorugh/views.py
+++ backend/pugorugh/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView as CA_view
 
 from .serializers import UserSerializer
 
 
-class UserRegisterView(CreateAPIView):
+class UserRegisterView(CA_view):
     permission_classes = (permissions.AllowAny,)
     model = get_user_model()
     serializer_class = UserSerializer
