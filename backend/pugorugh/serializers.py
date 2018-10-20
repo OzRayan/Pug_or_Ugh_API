3a6ee1a5203d705582984a8db2a2152e62eb3cbe
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-10-20 21:09:00 +0100

    upate

--- backend/pugorugh/serializers.py
+++ backend/pugorugh/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import Dog, UserPref
+from .models import Dog, UserDog, UserPref
 
 
 class UserSerializer(serializers.ModelSerializer):
